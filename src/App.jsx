4b99513488a9064f3b7bda317c68f571d23a78a0
unknown <singhaman2122@ternaengg.ac.in>
2024-11-22 15:22:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import { languages } from "./languages"
+
+
+export default function AssemblyEndgame() {
+
+  const [currentWord, setCurrentWord] = useState("react")
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  const languageElements = languages.map(lang => {
+      const styles = {
+          backgroundColor: lang.backgroundColor,
+          color: lang.color
+      }
+      return (
+          <span
+              className="chip"
+              style={styles}
+              key={lang.name}
+          >
+              {lang.name}
+          </span>
+      )
+  })
+  
+  const letterElements = currentWord.split("").map((letter, index) => (
+      <span key = {index}>{letter.toUpperCase()}</span>
+  ))
+  console.log(letterElements)
+
+  return (
+      <main>
+          <header>
+              <h1>Assembly: Endgame</h1>
+              <p>Guess the word within 8 attempts to keep the
+              programming world safe from Assembly!</p>
+          </header>
+          <section className="game-status">
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+          </section>
+          <section className="language-chips">
+              {languageElements}
+          </section>
+          <section className="word">
+              {letterElements}
+          </section>
+      </main>
+  )
+}
+
